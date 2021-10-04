8353528b6e4e64aa7a9b9fe81d3ef5452c711627
CodeJW <kjwboa@naver.com>
2021-10-04 18:17:46 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -9,4 +9,25 @@ document.addEventListener('scroll',() => {
   } else {
     navbar.classList.remove('navbar--dark');
   }
-})
+});
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=> {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  } 
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+  console.log(event.target.dataset.link);
+});
+
+/*
+//Scrolling when click the button
+const contactBtn = documnet.query.Selector('.home__contact');
+document.addEventListener('click', ()=>{
+
+})*/
