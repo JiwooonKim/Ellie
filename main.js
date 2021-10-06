df4d1a525a6b7eae5845a06289f19a5c3ba4c0f1
CodeJW <kjwboa@naver.com>
2021-10-06 19:32:26 +0900

    JS: Arrow up button

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/*
+scrollIntoView(): 이 메소드는  scrollIntoView()가 호출된 요소가 사용자에게 표시되도록 요소의 상위 컨테이너를 스크롤 한다.
+*/
+
+//scroll
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
+
 //Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -10,12 +21,6 @@ document.addEventListener('scroll',() => {
     navbar.classList.remove('navbar--dark');
   }
 });
-
-//scroll
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
 
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -40,4 +45,23 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+
+//Show scroll down when click arrow button
+const arrowUpBtn = document.querySelector('#arrowUpBtn');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight /  2){
+    arrowUpBtn.classList.add('visible');
+  } else {
+    arrowUpBtn.classList.remove('visible');
+  }
+});
+
+//Handle click on the "arrow up" button
+arrowUpBtn.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+
+
 
